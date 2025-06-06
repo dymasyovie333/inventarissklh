d2aca1cf4ae030fb1c692ab64faaa8657a2d431c
dymasyovie333 <yoviedymas@gmail.com>
2025-06-06 16:54:05 +0700

    Update firebase-config.ts

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "ISI_DI_SINI",
-  authDomain: "ISI_DI_SINI",
-  projectId: "ISI_DI_SINI",
-  storageBucket: "ISI_DI_SINI",
-  messagingSenderId: "ISI_DI_SINI",
-  appId: "ISI_DI_SINI"
+  apiKey: "AIzaSyCXBt47yyeZDXypNloLv6gsK86oREFpTV0",
+  authDomain: "inventarissklh.firebaseapp.com",
+  projectId: "inventarissklh",
+  storageBucket: "inventarissklh.firebasestorage.app",
+  messagingSenderId: "227861200621",
+  appId: "1:227861200621:web:c2fdb04adce809f66dd6e4",
+  measurementId: "G-MWTM5HSE4B"
 };
 
 const app = initializeApp(firebaseConfig);
